6c74be1febf40bc20f2db3fc24fbd6a398fe8922
Justin Nicholas Dubin <99613905+Jdubin1417@users.noreply.github.com>
2023-04-12 09:11:33 -0400

    Add files via upload

---
+++ lab9/BankAccount.cs
@@ -0,0 +1,117 @@
+﻿/**
+*--------------------------------------------------------------------
+* File name: BankAccount.cs
+* Project name: lab9
+*--------------------------------------------------------------------
+* Author’s name and email: Justin Dubin, dubinj@etsu.edu
+* Course-Section: CSCI 1250-001
+* Creation Date: 20 Apr 2022
+*
+-------------------------------------------------------------------
+*/
+public class BankAccount
+{
+    private int accountNumber;
+    private char type;
+    private decimal balance;
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    //constructors
+    //parameterized constructor
+    public BankAccount(char type, decimal initialDeposit, int accountNumber, string firstName, string lastName)
+    {
+       this.type = type;
+       this.balance = initialDeposit;
+       this.accountNumber = accountNumber;
+       FirstName = firstName;
+       LastName = lastName;
+    }
+
+    //copy constructor
+    public BankAccount(BankAccount obj)
+    {
+        this.type = obj.type;
+        this.balance = obj.balance;
+        this.accountNumber = obj.accountNumber;
+        FirstName = obj.FirstName;
+        LastName = obj.LastName;
+    }
+
+    public int GetAccountNumber() {return accountNumber; }
+    public decimal GetBalance() {return balance; }
+    
+    public string GetAccountTypeAsAString() 
+    {
+       if(type == 'C' || type == 'c') {return "Checking Account"; }
+       else if(type == 'S' || type == 's') {return "Savings Account"; } 
+       else return "Money Market Account (MMA)"; 
+    }
+   
+    public decimal GetMonthlyInterestRate()
+    {
+        if(type == 'C' || type == 'c') return 0.5M;
+       else if(type == 'S' || type == 's') return 2.4M; 
+       else return 8.0M;
+    }
+
+    public bool Deposit(decimal amount)
+    {
+        if(amount <= 0) {return false; }
+        
+            balance += amount;
+            return true;
+        
+    }
+
+    public bool Withdraw(decimal amount)
+    {
+        if(amount <= 0) {return false; }
+
+        //Savings Account logic
+        if (type == 'S')
+        {
+            if((balance - amount) < 5.0M) {return false; } 
+            // savings withdrawal amounts cannot exceed $500.00
+            if(amount > 500) { return false; }
+        }
+
+        //MMA Account Logic
+        if(type == 'M')
+        {
+            //MMA Must always have at least 1000.00 minimum balance
+            if ((balance - amount) < 1000.0M) {return false; }
+            //MMA Withdrawals cannot exceed 250.00
+            if(amount > 250.0M) {return false; }
+        }
+
+        balance -= amount;
+        return true;
+    }
+    
+    public decimal MonthlyInterest()
+    {
+        var addedInterest = balance * (GetMonthlyInterestRate()/100);
+        balance += addedInterest;
+        return addedInterest;
+    }
+
+    public override bool Equals(object obj)
+    {
+        BankAccount accountToCompare = (BankAccount)obj;
+        return type == accountToCompare.type && 
+            accountNumber == accountToCompare.accountNumber &&
+            balance == accountToCompare.balance;
+    }
+
+    public override string ToString()
+    {
+        string accountString = "";
+        accountString += $"Name:\t\t\t{FirstName} {LastName}\n";
+        accountString += $"Account Number:\t\t{accountNumber}\n";
+        accountString += $"Account Type:\t\t{GetAccountTypeAsAString()}\n";
+        accountString += $"Current Balance:\t${balance}\n";
+        accountString += $"Monthly Interest Rate:\t{GetMonthlyInterestRate()}\n";
+        return accountString;
+    }
+}
---
+++ lab9/Program.cs
@@ -0,0 +1,70 @@
+﻿/**
+*--------------------------------------------------------------------
+* File name: Program.cs
+* Project name: lab9
+*--------------------------------------------------------------------
+* Author’s name and email: Justin Dubin, dubinj@etsu.edu
+* Course-Section: CSCI 1250-001
+* Creation Date: 20 Apr 2022
+*
+-------------------------------------------------------------------
+*/
+int accountNumber = 1;
+
+decimal initialDeposit = 0.0M;
+do{
+    System.Console.Write("Enter an initial Deposit: ");
+    initialDeposit = Convert.ToDecimal(Console.ReadLine());
+} while (initialDeposit < 0.0M);
+
+System.Console.Write("Enter your First Name: ");
+string firstName = Console.ReadLine();
+
+System.Console.Write("Enter your Last Name: ");
+string lastName = Console.ReadLine();
+
+char type;
+if (initialDeposit >= 1000.0M) {type = 'M'; }
+else if (initialDeposit >= 5.0M) {type = 'S'; }
+else type = 'C';
+
+BankAccount myAccount = new BankAccount(type, initialDeposit, accountNumber++, firstName, lastName);
+
+//deposit an invalid amount
+if(myAccount.Deposit(-5.0M) == false) {Console.WriteLine("Invalid Deposit Amount -- cannot deposit a negative amount"); }
+
+//wiothdraw a negative amount
+if(myAccount.Withdraw(-5.0M) == false) {Console.WriteLine("Invalid withdrawal: Cannot withdraw negative amount");}
+
+//withdraw an amount > than the balance
+if(myAccount.Withdraw(myAccount.GetBalance()+1) == false) { Console.WriteLine("Invalid withdrawal"); }
+
+//deposit valid amount
+myAccount.Deposit(27.50M);
+System.Console.WriteLine("===== After Depositing 27.50 =====");
+System.Console.WriteLine(myAccount);
+
+//withdraw valid amount
+myAccount.Withdraw(15.40M);
+System.Console.WriteLine("===== After Withdrawing 15.40 =====");
+System.Console.WriteLine(myAccount);
+
+//calculate interest after 4 months
+for(int i = 0; i < 4; i++)
+{
+    System.Console.WriteLine($"Interest for month {i + 1}:\t{myAccount.MonthlyInterest()}");
+}
+System.Console.WriteLine("=====After 4 months interest =====");
+System.Console.WriteLine(myAccount);
+
+BankAccount copyOfMyAccount = new BankAccount(myAccount);
+System.Console.WriteLine("=====Copy of MyAccount=====");
+System.Console.WriteLine(copyOfMyAccount);
+
+copyOfMyAccount.LastName = "Hall";
+//should be true
+System.Console.WriteLine(copyOfMyAccount.Equals(myAccount));
+
+copyOfMyAccount.Withdraw(50.00M);
+//should be false
+System.Console.WriteLine(copyOfMyAccount.Equals(myAccount));
